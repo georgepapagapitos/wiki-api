468add74889bef26b7627a561300e5ac1bd84a9a
George Papagapitos <george.g.papagapitos@gmail.com>
2021-05-14 10:14:39 -0500

    idk

--- client/src/pages/ArticlesList.jsx
+++ client/src/pages/ArticlesList.jsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from 'react';
+import ReactTable from 'react-table';
+import api from '../api';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 0 40px 40px 40px;
+`
 
 export default function ArticlesList() {
+
+  const [articles, setArticles] = useState([]);
+  const [columns, setColumns] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(async () => {
+    setIsLoading(true);
+    await api.getAllArticles().then(articles => {
+      console.log(articles);
+    });
+  }, []);
+
   return (
     <div>
       <p>Movie List Page</p>
